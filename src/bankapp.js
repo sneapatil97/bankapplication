d15b41001675556aabaf7071ba81eded2f166228
sneapatil97 <patilssneha97@gmail.com>
2021-05-05 12:14:45 +0530

    bank application

--- src/bankapp.js
+++ src/bankapp.js
@@ -1,100 +1,116 @@
-import React,{Component, Componet} from 'react';
+import React, { Component, Componet } from 'react';
 import { MDBDataTable } from 'mdbreact';
 import axios from 'axios';
 
-class BankApplication extends Component
-{
-    constructor(props)
-    {
+class BankApplication extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            data:'',
+        this.state = {
+            data: ''
         }
+         this.cityname=''
     }
 
-    componentDidMount()
-    {
+    componentDidMount() {
         this.fetchBankDetails();
     }
 
-     fetchBankDetails=()=>
-     {
-        const url="https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI";
+    Bankcity = (obj) => {
+        
+        this.cityname= obj.target.value
+        this.fetchBankDetails();
+    }
+
+    fetchBankDetails = () => {
+        let url="";
+        switch (this.cityname) {
+            case "Pune": url = "https://vast-shore-74260.herokuapp.com/banks?city=PUNE";
+                break;
+            case "Banglore":url = "https://vast-shore-74260.herokuapp.com/banks?city=BANGALORE";
+                break;
+            case "Kolkata":url = "https://vast-shore-74260.herokuapp.com/banks?city=KOLKATA";
+                break; 
+            case "Delhi":url = "https://vast-shore-74260.herokuapp.com/banks?city=DELHI";
+                break;
+            default: url = "https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI";
+        }
         axios.get(url)
-        .then(res=>{
-            this.setState({
-                data:{
-                    columns:[
-                        {
-                            label:'IFSC Number',
-                            field:'ifsc',
-                            sort:'asc'
-                        },
+            .then(res => {
+                this.setState({
+                    data: {
+                        columns: [
+                            {
+                                label: 'IFSC Number',
+                                field: 'ifsc',
+                                sort: 'asc'
+                            },
 
-                        {
-                            label:'Bank Id',
-                            field:'bank_id',
-                            sort:'asc'
-                        },
-                        {
-                            label:'Branch',
-                            field:'branch',
-                            sort:'asc'
-                        },
-                        {
-                            label:'Address',
-                            field:'address',
-                            sort:'asc'
-                        },
-                        {
-                            label:'City',
-                            field:'city',
-                            sort:'asc'
-                        },
-                        {
-                            label:'District',
-                            field:'district',
-                            sort:'asc'
-                        },
-                        {
-                            label:'State',
-                            field:'state',
-                            sort:'asc'
-                        },
-                        {
-                            label:'Bank Name',
-                            field:'bank_name',
-                            sort:'asc'
-                        },
-                    ],
-                    
-                    rows:res.data
-                }
+                            {
+                                label: 'Bank Id',
+                                field: 'bank_id',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'Branch',
+                                field: 'branch',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'Address',
+                                field: 'address',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'City',
+                                field: 'city',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'District',
+                                field: 'district',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'State',
+                                field: 'state',
+                                sort: 'asc'
+                            },
+                            {
+                                label: 'Bank Name',
+                                field: 'bank_name',
+                                sort: 'asc'
+                            },
+                        ],
+
+                        rows: res.data
+                    }
+                })
             })
-        })
-        .catch(err=> console.log(err));
-     }
+            .catch(err => console.log(err));
+    }
 
-     render()
-     {
-         return(
-             <div className="container mt-2">
-                 <div className="row">
-                     <div className="col-lg-6">
-                         <h3 className="text-primary">Bank Branches</h3>
-                     </div>
-                     <div className="col-lg-4 form-group">
-                         <select className="form-control">
-                            <option>Mumbai</option>
-                            <option>Pune</option>
-                            <option>Banglore</option>
-                            <option>Kolkata</option>
-                            <option>Delhi</option>
-                         </select>
-                     </div>
-                 </div>
-                 <div className="row mt-5">
-                     <div className="col-lg-12">
+    render() {
+        return (
+            <div className="container mt-2">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h3 className="text-primary">Bank Branches</h3>
+                    </div>
+                    <div className="col-lg-4 form-group">
+                    <form onSubmit={this.handleSubmit}>
+                        <select className="form-control" value={this.state.value} onChange={this.Bankcity.bind(this)}>
+                            <option value="Mumbai">Mumbai</option>
+                            <option value="Pune">Pune</option>
+                            <option value="Banglore">Banglore</option>
+                            <option value="Kolkata">Kolkata</option>
+                            <option value="Delhi">Delhi</option>
+                        </select>
+                        </form>
+                    </div>
+                
+                </div>
+                <div className="row mt-5">
+                    <div className="col-lg-12">
                         <MDBDataTable
                             striped
                             bordered
@@ -103,12 +119,13 @@ class BankApplication extends Component
                             paging={true}
                             searching={true}
                         />
-                     </div>
-                 </div>
-             </div>
-            
-         )
-     }
+                    </div>
+                </div>
+            </div>
+
+
+        )
+    }
 }
 
 export default BankApplication;
